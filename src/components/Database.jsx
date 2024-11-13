57ac767ae8ee36e17d75fd84037179d8390ae820
maah122 <neilmatthew.palania@g.msuiit.edu.ph>
2024-11-13 17:21:04 +0800

    sample

--- src/components/Database.jsx
+++ src/components/Database.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Database = () => {
   return (
     <div className="service-page">
-      <h2>Yow</h2>
+      <h2>Yowaywa si neil</h2>
       <p>This is the Database.</p>
     </div>
   );
